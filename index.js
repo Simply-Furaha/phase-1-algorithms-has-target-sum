3af66bb5df900fa5d9d7e0947a4b3fa100b5d842
Maxwell Furaha <maxwellfuraha91@gmail.com>
2024-07-13 13:01:17 +0300

    Big 0

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+// Function implementation
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = new Set();
+
+  for (const number of array) {
+    const complement = target - number;
+    if (seenNumbers.has(complement)) {
+      return true;
+    }
+    seenNumbers.add(number);
+  }
+
+  return false;
 }
+
+// Test cases
+console.log(hasTargetSum([3, 8, 12, 4, 11, 7], 10)); // true
+console.log(hasTargetSum([22, 19, 4, 6, 30], 25)); // true
+console.log(hasTargetSum([1, 2, 5], 4)); // false
+console.log(hasTargetSum([-7, 10, 4, 8], 3)); // true
+console.log(hasTargetSum([1, 2, 3, 4], 5)); // true
+console.log(hasTargetSum([2, 2, 3, 3], 4)); // true
+console.log(hasTargetSum([4], 4)); // false
+
 
 /* 
   Write the Big O time complexity of your function here
